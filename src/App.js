a42a68552383b6145c37051ea687bbfb85673c40
Omar <Omar>
2022-02-07 19:44:09 -0500

    random bootstrap styling to test it out

--- src/App.js
+++ src/App.js
@@ -1,12 +1,189 @@
-import './App.css';
-
-
+import "./App.css";
+import Button from "react-bootstrap/Button";
+import Nav from "react-bootstrap/Nav";
+import Form from "react-bootstrap/Form";
+import Table from "react-bootstrap/Table";
+import Card from "react-bootstrap/Card";
 
 function App() {
   return (
     <div className="App">
+      <Nav activeKey="/home">
+        <Nav.Item>
+          <Nav.Link href="/home">Home</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="/about">About Us</Nav.Link>
+        </Nav.Item>
+      </Nav>
       <h1>Bootstrap Website</h1>
-     
+      <br />
+      <Button variant="danger">Danger</Button>
+      <br />
+      <br />
+      <Form>
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+        <br />
+        <Form.Group controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        <br />
+        <Form.Group controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Check me out" />
+        </Form.Group>
+        <br />
+        <Form.Group>
+          <Form.Label>Form Dropdown with favorite movies</Form.Label>
+          <Form.Control as="select">
+            <option>Inception</option>
+            <option>Forrest Gump</option>
+            <option>Shawshank Redemption</option>
+            <option>Spiderman: Into the Spiderverse</option>
+            <option>Avengers: Endgame</option>
+          </Form.Control>
+        </Form.Group>
+        <br />
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+      <br />
+      <div>
+        <Table striped bordered hover variant="dark">
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Username</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>Mark</td>
+              <td>Otto</td>
+              <td>@mdo</td>
+            </tr>
+            <tr>
+              <td>2</td>
+              <td>Jacob</td>
+              <td>Thornton</td>
+              <td>@fat</td>
+            </tr>
+            <tr>
+              <td>3</td>
+              <td colSpan={2}>Larry the Bird</td>
+              <td>@twitter</td>
+            </tr>
+          </tbody>
+        </Table>
+      </div>
+      <br />
+      <>
+        <Card border="primary" style={{ width: "18rem" }}>
+          <Card.Header>Header</Card.Header>
+          <Card.Body>
+            <Card.Title>Primary Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        <br />
+
+        <Card border="secondary" style={{ width: "18rem" }}>
+          <Card.Header>Header</Card.Header>
+          <Card.Body>
+            <Card.Title>Secondary Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        <br />
+
+        <Card border="success" style={{ width: "18rem" }}>
+          <Card.Header>Header</Card.Header>
+          <Card.Body>
+            <Card.Title>Success Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        <br />
+
+        <Card border="danger" style={{ width: "18rem" }}>
+          <Card.Header>Header</Card.Header>
+          <Card.Body>
+            <Card.Title>Danger Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        <br />
+
+        <Card border="warning" style={{ width: "18rem" }}>
+          <Card.Header>Header</Card.Header>
+          <Card.Body>
+            <Card.Title>Warning Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        <br />
+
+        <Card border="info" style={{ width: "18rem" }}>
+          <Card.Header>Header</Card.Header>
+          <Card.Body>
+            <Card.Title>Info Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        <br />
+
+        <Card border="dark" style={{ width: "18rem" }}>
+          <Card.Header>Header</Card.Header>
+          <Card.Body>
+            <Card.Title>Dark Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        <br />
+
+        <Card border="light" style={{ width: "18rem" }}>
+          <Card.Header>Header</Card.Header>
+          <Card.Body>
+            <Card.Title>Light Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        <br />
+      </>
     </div>
   );
 }
